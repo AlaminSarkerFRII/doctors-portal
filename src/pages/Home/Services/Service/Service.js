afade7b5bd8f4a6e62a600f05144d3063fc0f743
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-10 23:58:39 +0600

    footer added

--- src/pages/Home/Services/Service/Service.js
+++ src/pages/Home/Services/Service/Service.js
@@ -3,17 +3,12 @@ import DentalBanner from "../DentalBanner/DentalBanner";
 
 const Service = ({ service }) => {
   return (
-    <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-      <figure class="px-10 pt-10">
-        <img
-          className="w-24 h-24"
-          src={service.img}
-          alt="Shoes"
-          class="rounded-xl"
-        />
+    <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img className="w-24 h-24 rounded-xl" src={service.img} alt="Shoes" />
       </figure>
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">{service.name}</h2>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{service.name}</h2>
         <p>{service.description}</p>
       </div>
     </div>
--- src/pages/Testimonials/Review/Review.js
+++ src/pages/Testimonials/Review/Review.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const Review = ({ review }) => {
   return (
-    <div class="card lg:w-lg bg-base-100 shadow-xl">
-      <div class="card-body">
+    <div className="card lg:w-lg bg-base-100 shadow-xl">
+      <div className="card-body">
         <p>{review.review}</p>
-        <div class="flex items-center ">
+        <div className="flex items-center ">
           <div className="avatar online mr-10">
             <div className="w-24 rounded-full ring ring-primary ring-offset-base-100">
               <img src={review.img} alt="" />
             </div>
           </div>
           <div>
-            <h2 class="card-title">{review.name}</h2>
+            <h2 className="card-title">{review.name}</h2>
             <p>{review.location}</p>
           </div>
         </div>
