4a10c430acead0da81b9e8bbac28986f473e7ff2
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-11 02:09:12 +0600

    fake services added

--- src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
 const AvailableAppointment = ({ selected }) => {
+  // set services
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json()")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div>
       <h2 className="text-secondary text-center font-bold text-3xl">
         Available Appointment on :{format(selected, "PP")}
       </h2>
+      <div>{services.map()}</div>
     </div>
   );
 };
