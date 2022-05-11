d7a1d2b7b0fd9d85224f593d8a9ac5fc30a06520
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-12 01:49:04 +0600

    implement google sign in

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Login = () => {
+  //sign in with google
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    console.log(user);
+  }
+
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero-content flex-col lg:flex-row-reverse justify-center items-center">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
           <p className="py-6">
@@ -14,10 +34,10 @@ const Login = () => {
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
+            <div className="text-center text-2xl font-bold">
+              <h2>LOGIN</h2>
+            </div>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
               <input
                 type="text"
                 placeholder="email"
@@ -25,23 +45,29 @@ const Login = () => {
               />
             </div>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
               <input
                 type="text"
                 placeholder="password"
                 className="input input-bordered"
               />
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
+                <Link
+                  to="/"
+                  className="label-text-alt link link-hover text-warning font-bold"
+                >
                   Forgot password?
-                </a>
+                </Link>
               </label>
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
+          </div>
+          <div class="divider">OR</div>
+          <div className="text-center py-5">
+            <button onClick={() => signInWithGoogle()} class="btn btn-outline ">
+              SignIn With Google
+            </button>
           </div>
         </div>
       </div>
