4541b6e642d786390aa4daa81d4bcc25227355a7
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-13 00:50:34 +0600

    login form implement

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Appointment from "./pages/Appointment/Appointment/Appointment";
 import About from "./pages/Home/About/About";
 import Home from "./pages/Home/Home/Home";
-import Login from "./pages/Login/Login";
+import Login from "./pages/Login/Login/Login";
 import Navbar from "./pages/Shared/Navbar/Navbar";
 
 function App() {
