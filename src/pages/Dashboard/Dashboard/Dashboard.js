b0d172698019fd62e5ccd90c05a11eb618d84d5e
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-14 20:57:15 +0600

    table added in myappointsment

--- src/pages/Dashboard/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard/Dashboard.js
@@ -3,21 +3,23 @@ import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div className="drawer drawer-mobile">
+    <div className="drawer drawer-mobile py-4">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <h2 className="text-2xl text-purple-700">Welcome to your Dashboard</h2>
+        <h2 className="text-2xl text-purple-700 text-center">
+          Welcome to your Dashboard
+        </h2>
         <Outlet />
       </div>
       <div className="drawer-side">
         <label for="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
           <li>
-            <Link to="/dashboard">Sidebar Item 1</Link>
+            <Link to="/dashboard">My Appointments </Link>
           </li>
           <li>
-            <Link to="/dashboard/myreview">Sidebar Item 2</Link>
+            <Link to="/dashboard/myreview">My Reviews </Link>
           </li>
         </ul>
       </div>
--- src/pages/Dashboard/MyAppointments/MyAppointments.js
+++ src/pages/Dashboard/MyAppointments/MyAppointments.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const MyAppointments = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+    }
+  }, [user]);
+
   return (
-    <div>
-      <h2>My Appointments </h2>
+    <div className="mb-2">
+      <h2 className="text-center">My Appointments {appointments.length} </h2>
+
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>treatment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((appointment, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{appointment.patientname}</td>
+                <td>{appointment.date}</td>
+                <td>{appointment.slot}</td>
+                <td>{appointment.treatment}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
